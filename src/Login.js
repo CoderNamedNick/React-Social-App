517d72953e835ce5557f3210f2c28d3f5e43175d
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-28 11:34:31 -0400

    dleted defaults making login page

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+
+
+  return (
+    <div className="main-login-page-div">
+      <div className="Login-square">
+        <h2>SUPER COOL NAME OF SITE</h2>
+        <br></br>
+        <input type="text"></input>
+        <input type="password"></input>
+        <h2>LOG IN</h2>
+        <p>or</p>
+        <h3>Sign Up</h3>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
