fc9334d166d34e00f7f466b6bdd48389d8d9ee0c
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-04-04 10:51:47 -0400

    made future plans for profile book and made style changes

--- src/ProfileBook.js
+++ src/ProfileBook.js
@@ -5,6 +5,8 @@ const ProfileBook = () => {
  // Use Prop from app to disply User data
  //if on other persons profile ur id params to get there info and with a fetch method
 
+ // when edit pfp is clicked firsrt make input boxes for bio, daily, and maybe UserName then make a patch request with new info
+
   return( 
     <div>
       <div>
@@ -14,7 +16,10 @@ const ProfileBook = () => {
           <h3>Daily Obj</h3>
           <p>Bio</p>
           <p>Traveler Since: adadadad</p>
+          <h4>Edit ProfileBook</h4>
         </div>
+        <br></br>
+        <br></br>
         <div>
           <h2>Guilds Traveler is part of</h2>
           {/*loop through user.Guilds array and then display guilds in this format */}
