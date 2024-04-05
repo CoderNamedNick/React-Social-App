beaab568222a3c8e9a9eba2671e9b2ef7dbd14a4
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-04-05 18:36:47 -0400

    made fecth with login and it works

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from './images/Tavern-logo.png'
 
-const LoginPage = ({onSignupClick}) => {
+const LoginPage = ({ onSignupClick, onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const navigate = useNavigate(); // Initialize useNavigate
 
-  {/*useEffect(() => {
-    // Fetch message from backend when component mounts
-    fetch('http://localhost:5000/api/message')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching message:', error));
-  }, []);*/}
+  const handleLogin = () => {
+    // Send a POST request to your backend server with user credentials
+    fetch('http://localhost:5000/Users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+    .then(response => {
+      if (response.ok) {
+        // If response is OK, user is authenticated
+        setMessage('Login successful');
+        onLogin();
+        // Navigate to home page
+        navigate('/HomePage');
+      } else {
+        // If response is not OK, handle error
+        setMessage('Incorrect email or password');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      setMessage('An error occurred, please try again later');
+    });
+  };
 
   return (
     <div className="main-login-page-div">
       <img src={Logo} alt="TAVERN"></img>
       <div className="Login-square">
         <br/>
-        <input className="LoginBars" type="text" placeholder="Email"></input>
-        <input className="LoginBars" type="password"placeholder="Pasword"></input>
-        <h2 className="loginlink medievalsharp-regular">LOG IN!</h2>
+        <input className="LoginBars" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <input className="LoginBars" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+        <h2 className="loginlink medievalsharp-regular" onClick={handleLogin}>LOG IN!</h2>
         <p className="new-p-login medievalsharp-regular">New? <Link to="/SignUp"><span className="signuplink" onClick={onSignupClick}>Sign Up</span></Link></p>
       </div>
       <p>{message}</p> {/* Display the message from backend */}
