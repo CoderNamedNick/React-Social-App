0de865f1f1c44283bf0f698d77d0bc0dac2a50ac
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-04-08 11:32:12 -0400

    making find travelers component

---
+++ src/FindTravelers.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FindTravelers = ({UserData, setUserData}) => {
+  // make a feild that gives users option to make his accout findable make true by deafult
+  // make a grid of already make friends 
+  //make a grid of findable poeple with the get all but priotise people with age closest to user
+  // have grid items show Username, Daliy, and accdate for now
+  // make other component for other travelers profiles books
+
+
+
+
+return(
+  <div>
+
+  </div>
+)
+}
+
+export default FindTravelers
