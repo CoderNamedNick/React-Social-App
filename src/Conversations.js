84885170740cb7bc882275e16401d39d006fc5a9
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-04-28 10:47:06 -0400

    made convo start pop up

--- src/Conversations.js
+++ src/Conversations.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Conversations = ({ UserData, setUserData }) => {
   const [noCompanions, setNocompanions] = useState(false);
   const [Conversations, setConversations] = useState([]);
   const [companionsData, setCompanionsData] = useState([]);
-
+  const [showConvoWindow, setshowConvoWindow] = useState(false);
+  const [Convocompanion, setConvocompanion] = useState('');
+  const [Convocompanionid, setConvocompanionid] = useState('');
 
   useEffect(() => {
     fetch(`http://localhost:5000/Messages/Conversations/${UserData.id || UserData.id}`)
@@ -55,37 +58,83 @@ const Conversations = ({ UserData, setUserData }) => {
     const userData = await response.json();
     return userData;
   };
+  const cancelconvo = () => {
+    setshowConvoWindow(false)
+  }
+  const startconvoclick = (companionName, companionid) => {
+    setshowConvoWindow(!showConvoWindow)
+    setConvocompanion(companionName)
+    setConvocompanionid(companionid)
+  }
+
+  const StartNewConvo = () => {
+
+  }
 
   return (
     <div className='Conversations-main-div'>
-      {noCompanions && (
-        <div style={{paddingTop: '114px'}}>
+     {!showConvoWindow && noCompanions && (
+        <div className='Convos-div'>
           <h1>You have No Conversations</h1>
-          <br/>
-          <br/>
-          <br/>
+          <br />
+          <br />
+          <br />
           <h2>Make a Conversation with a companion</h2>
-          {companionsData.map(({ userData}, index) => (
-            <div key={userData._id || userData.id} >
-              <p style={{paddingTop: '10px'}}>{userData.username}</p>
-              <p style={{wordWrap: 'break-word', marginTop: '-10px'}}>Daily: {userData.dailyObj}</p>
-            </div>
-          ))}
+          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '7px', width: '98%',}}>
+            {companionsData.map(({ userData }, index) => (
+              <div className='Convo-companion-map-div' key={userData._id || userData.id} >
+                <h3>{userData.username}</h3>
+                <p style={{ wordWrap: 'break-word', marginTop: '-10px' }}>Daily: {userData.dailyObj}</p>
+                <div style={{display: 'flex', flexDirection: 'row', gap: '15px', justifyContent: 'space-between' }}>
+                  <Link key={userData._id || userData.id} to={`/user/${userData.username}`}><div>View Profile</div></Link>
+                  <div style={{cursor: 'pointer'}} onClick={() => {startconvoclick(userData.username, userData._id || userData.id)}}>Make A Conversation</div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       )}
-      {!noCompanions && (
+      {!showConvoWindow && !noCompanions && (
         <div style={{paddingTop: '114px'}}>
-          <h1>Current Conversations</h1>
-          {Conversations.map(Convo => {
-            // Filter out the user's own ID from the messengers list
-            const otherMessengers = Convo.messengers.filter(messengerId => messengerId !== UserData.id && messengerId !== UserData._id);
-            
-            return (
-              <div style={{backgroundColor: 'white'}} key={Convo.id || Convo._id}>
-                <h3>Convo With: {otherMessengers.join(', ')}</h3>
+          <div>
+            <h1>Current Conversations</h1>
+            {Conversations.map(Convo => {
+              // Filter out the user's own ID from the messengers list
+              const otherMessengers = Convo.messengers.filter(messengerId => messengerId !== UserData.id && messengerId !== UserData._id);
+              
+              return (
+                <div style={{backgroundColor: 'white'}} key={Convo.id || Convo._id}>
+                  <h3>Convo With: {otherMessengers.join(', ')}</h3>
+                </div>
+              );
+            })}
+          </div>
+          <br />
+          <br />
+          <h2>Make A New Conversation</h2>
+          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '7px', width: '98%',}}>
+            {companionsData.map(({ userData }, index) => (
+              <div className='Convo-companion-map-div' key={userData._id || userData.id} >
+                <h3>{userData.username}</h3>
+                <p style={{ wordWrap: 'break-word', marginTop: '-10px' }}>Daily: {userData.dailyObj}</p>
+                <div style={{display: 'flex', flexDirection: 'row', gap: '15px', justifyContent: 'space-between' }}>
+                  <Link key={userData._id || userData.id} to={`/user/${userData.username}`}><div>View Profile</div></Link>
+                  <div style={{cursor: 'pointer'}} onClick={startconvoclick}>Make A Conversation</div>
+                </div>
               </div>
-            );
-          })}
+            ))}
+          </div>
+        </div>
+      )}
+      {showConvoWindow && (
+        <div className='convo-window-main-div'>
+          <div>
+            <h1 style={{marginLeft: '50px', paddingTop: '50px'}}>Sending To: {Convocompanion}</h1>
+            <br/>
+            <textarea style={{marginLeft: '50px', backgroundColor: '#d8d8d89a', fontSize: '30px', minWidth: '850px', minHeight: '550px', maxWidth: '850px', maxHeight: '550px'}}></textarea>
+          </div>
+          <h2 style={{cursor: 'pointer', position: 'absolute', left: '10px', bottom: '0'}} onClick={cancelconvo}>Cancel Message</h2>
+          <h1 style={{cursor: 'pointer', position: 'absolute', right: '10px', bottom: '0'}} onClick={cancelconvo}>Send Message</h1>
         </div>
       )}
     </div>
