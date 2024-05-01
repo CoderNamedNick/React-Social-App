e3d3f18b42aafe2a7deb84e1b82b6748578344a3
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-05-01 09:50:25 -0400

    makeing plans for messages

---
+++ src/Messages.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+
+const Messages = () => {
+  const [ConversationsArray, setConversationsArray] = useState([]);
+  const [CurrentConvo, setCurrentConvo] = useState(null);
+  const [messagesArray, setmessagesArray] = useState([])
+
+  //can just do a normal fetch for convos at first on load
+  //set up socket for incoming messages and convos made by other companions
+  
+  return (
+    <div className="main-messages-div">
+      <div>{/*Make this have padding top with flexdir row */}
+        <div className="left-side-with-Converstaions">
+          {/*loop thru array after fetch  post convos on left side with username of person*/}
+        </div>
+        <div className="right-side-with-messages">
+          {/*on click of convo get new messages with messages box and a array getting looped thru newest messages*/}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Messages
