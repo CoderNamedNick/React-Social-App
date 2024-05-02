4dd82226e2b08b96f3791d1c6fe24701c76af1f7
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-05-02 10:12:44 -0400

    Update Messages.js

--- src/Messages.js
+++ src/Messages.js
@@ -4,8 +4,39 @@ import { useState, useEffect } from "react";
 const Messages = ({ UserData, setUserData, ClickedConvo, setClickedConvo }) => {
   const [ConversationsArray, setConversationsArray] = useState([]);
   const [CurrentConvo, setCurrentConvo] = useState(null);
+  const [NoCurrentConvo, SetNoCurrentConvo] = useState(false)
   const [messagesArray, setmessagesArray] = useState([])
 
+  useEffect(() => {
+    //setting convo companion with what convo was clicked
+    console.log(ClickedConvo)
+    fetchConversations()
+    if (ClickedConvo === null) {
+      SetNoCurrentConvo(true)
+    } else{
+      setCurrentConvo(ClickedConvo)
+      //setClickedConvo(null)
+    }
+    // when getting cliked convo compare it to fetch new array and display it on launch
+  }, []);
+
+  const fetchConversations = () => {
+    fetch(`http://localhost:5000/Messages/Conversations/${UserData.id || UserData.id}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to fetch conversations');
+        }
+      })
+      .then(data => {
+        console.log(data.conversations);
+        setConversationsArray(data.conversations);
+      })
+    .catch(error => {
+      console.error('Error fetching conversations:', error);
+    });
+  };
   
 
   //can just do a normal fetch for convos at first on load
@@ -16,11 +47,20 @@ const Messages = ({ UserData, setUserData, ClickedConvo, setClickedConvo }) => {
       <div className="messages-div">{/*Make this have padding top with flexdir row */}
         <div className="left-side-with-Conversations">
           <h2>Conversations</h2>
+          {ConversationsArray.map(Convo => {
+              // Check if Convo.UserNames is defined before filtering
+              const otherUsernames = Convo.UserNames ? Convo.UserNames.filter(username => username !== UserData.username) : [];
+              return (
+                <div className='current-convos' key={Convo.id || Convo._id}>
+                  <h3>Convo With: {otherUsernames.join(', ')}</h3>
+                </div>
+              );
+            })}
         
           {/*loop thru array after fetch  post convos on left side with username of person*/}
         </div>
         <div className="right-side-with-messages">
-          <h1>CLick a convo</h1>
+          {NoCurrentConvo && (<h1>CLick a convo</h1>)}
 
           {/*on click of convo get new messages with messages box and a array getting looped thru newest messages*/}
         </div>
