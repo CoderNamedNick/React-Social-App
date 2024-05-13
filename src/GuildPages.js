bc68c85399937d6dcadbd2282d4467db164d2211
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-05-13 10:39:18 -0400

    making guild pages

--- src/GuildPages.js
+++ src/GuildPages.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { io } from "socket.io-client";
 
 const GuildPages = ({UserData, setUserData, clickedGuild, setclikedGuild}) => {
+  const [socket, setSocket] = useState(null);
   const [AllMembers, setAllMembers] = useState(null);
   const [clickedMember, setclickedMember] = useState(null);
 
@@ -14,23 +16,57 @@ const GuildPages = ({UserData, setUserData, clickedGuild, setclikedGuild}) => {
   // need original use effect fetch for guild data
   // need post functions with sockets
 
+  const fetchGuildMembers = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/Guilds/JoinedTravelers/${clickedGuild.id || clickedGuild._id}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch members');
+      }
+      const MembersData = await response.json();
+      console.log(MembersData)
+      setAllMembers(MembersData);
+    } catch (error) {
+      console.error('Error fetching all guilds:', error);
+    }
+  };
   useEffect(() => {
-    const fetchGuildMembers = async () => {
-      try {
-        const response = await fetch(`http://localhost:5000/Guilds//JoinedTravelers/${clickedGuild.id || clickedGuild._id}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch members');
-        }
-        const MembersData = await response.json();
-        console.log(MembersData)
-        setAllMembers(MembersData);
-      } catch (error) {
-        console.error('Error fetching all guilds:', error);
+     // Establish Socket connection
+     const socket = io('http://localhost:5000');
+     setSocket(socket)
+    // make a socket for notifs
+     socket.on('connect', () => {
+       console.log('connected');
+     });
+    
+     // Clean up socket connection when component unmounts
+    fetchGuildMembers();
+    return () => {
+      socket.disconnect();
+    };
+  }, []); 
+  useEffect(() => {
+    if (socket) {
+      socket.on('promote-update', (updatedGuild, guildMembersWithElders) => {
+        setAllMembers(guildMembersWithElders);
+      });
+    }
+  
+    // Clean up event listener when component unmounts
+    return () => {
+      if (socket) {
+        socket.off('promote-update');
       }
     };
-    fetchGuildMembers();
-  }, []); 
+  }, [AllMembers]);
   console.log(clickedGuild)
+
+  const PromoteToElder = (TravelerId) => {
+    // check if this works pls
+    if (socket) {
+      const GuildId = clickedGuild.id || clickedGuild._id
+      socket.emit('update-to-elder', GuildId, TravelerId)
+    }
+  }
   
   return (
     <div className='Guild-Pages-main-div'>
@@ -41,14 +77,31 @@ const GuildPages = ({UserData, setUserData, clickedGuild, setclikedGuild}) => {
         </div>
           {AllMembers && (
             <div style={{width: '98%'}}>
-              <h2>{AllMembers.Owner.UserName} <span style={{fontSize: '14px', fontWeight: '400'}}>Owner</span></h2>
+              <h2 style={{cursor: 'pointer'}} onClick={() => toggleTooltip(AllMembers.Owner.id)}>{AllMembers.Owner.UserName} <span style={{fontSize: '14px', fontWeight: '400'}}>Owner</span></h2>
+              {UserData.username !== AllMembers.Owner.UserName && (
+                <div className="guild-Owner-tooltip" style={{ display: clickedMember === AllMembers.Owner.id ? 'block' : 'none' }}>
+                  <Link to={`/user/${AllMembers.Owner.UserName}`}><div>View {AllMembers.Owner.UserName}'s Profile</div></Link> 
+                </div>
+              )}
               <hr/>
               <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
                 {AllMembers.Elders
                 .filter(elders => elders.AccPrivate === true) 
                 .map((elder) => (
                   <div>
-                    <h2>{elder.UserName}<span style={{fontSize: '14px', fontWeight: '400'}}>Elder</span></h2>
+                    <h2 style={{cursor: 'pointer'}} onClick={() => toggleTooltip(elder.id)}>{elder.UserName}<span style={{fontSize: '14px', fontWeight: '400'}}>Elder</span></h2>
+                    {UserData.username !== elder.UserName && (
+                      <div className="guild-elder-tooltip" style={{ display: clickedMember === elder.id ? 'block' : 'none' }}>
+                      <Link to={`/user/${elder.UserName}`}><div>View {elder.UserName}'s Profile</div></Link>
+                        {UserData.username === AllMembers.Owner.UserName && (
+                          <div>Demote to Member</div>
+                        )}
+                        <hr style={{margin: '3px'}}/>
+                        {UserData.username === AllMembers.Owner.UserName  && (
+                          <div>Ban From Guild</div>
+                        )}
+                      </div>
+                    )}
                   </div>
                 ))}
               </div>
@@ -61,16 +114,18 @@ const GuildPages = ({UserData, setUserData, clickedGuild, setclikedGuild}) => {
                   <h2 style={{cursor: 'pointer'}} onClick={() => toggleTooltip(member.id)}>
                     {member.UserName} <span style={{fontSize: '14px', fontWeight: '400'}}>Member</span>
                   </h2>
-                  <div className="guild-member-tooltip" style={{ display: clickedMember === member.id ? 'block' : 'none' }}>
-                    <div>View {member.UserName}'s Profile</div>
-                    {UserData.username === AllMembers.Owner.UserName && (
-                      <div>Promote to Elder</div>
-                    )}
-                    <hr style={{margin: '3px'}}/>
-                    {(UserData.username === AllMembers.Owner.UserName || AllMembers.Elders.includes(UserData.username)) && (
-                      <div>Ban From Guild</div>
-                    )}
-                  </div>
+                  {UserData.username !== member.UserName && (
+                    <div className="guild-member-tooltip" style={{ display: clickedMember === member.id ? 'block' : 'none' }}>
+                      <Link to={`/user/${member.UserName}`}><div>View {member.UserName}'s Profile</div></Link>
+                      {UserData.username === AllMembers.Owner.UserName && (
+                        <div onClick={() => {PromoteToElder(member.id || member._id)}}>Promote to Elder</div>
+                      )}
+                      <hr style={{margin: '3px'}}/>
+                      {(UserData.username === AllMembers.Owner.UserName || AllMembers.Elders.includes(UserData.username)) && (
+                        <div>Ban From Guild</div>
+                      )}
+                    </div>
+                  )}
                 </div>
               ))}
               </div>
