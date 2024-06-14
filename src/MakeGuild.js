9c31d9bc701051d6f9b6c8e30964adb11f6b4cef
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-06-14 09:37:34 -0400

    make guild now links to all and styles

--- src/MakeGuild.js
+++ src/MakeGuild.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const MakeGuild = ({ UserData, setUserData }) => {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -167,7 +168,7 @@ const MakeGuild = ({ UserData, setUserData }) => {
           <br/>
           <br/>
           <br/>
-          <h3 style={{textAlign: 'center', cursor: 'pointer'}}> Go To Guild Page </h3> 
+          <Link to="/All-Guilds"><h3 style={{textAlign: 'center', cursor: 'pointer'}}> Go To Guild Page </h3> </Link>
         </div>
       )}
     </div>
